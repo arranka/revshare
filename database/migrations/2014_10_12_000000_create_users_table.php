575b9c65d0082b0b87a6ab8495b907800e8f8d39
Jake Fell <jakefell@hotmail.co.uk>
2016-03-01 17:01:22 +0000

    Updated users table migration and User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+
+            $table->integer('referer_user_id')->unsigned()->nullable();
+            $table->foreign('referer_user_id')->references('id')->on('users');
+            
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
